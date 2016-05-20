a3d682d1c7fd927fe29ca80996db757ece3fa2f7
blkshdw <aleksey.glebow@gmail.com>
2016-05-21 01:06:43 +0300

    strange

--- src/js/components/users/UserCardReviews/UserCardReviews.js
+++ src/js/components/users/UserCardReviews/UserCardReviews.js
@@ -1,6 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import { Panel, Button, ButtonToolbar, Well } from 'react-bootstrap';
-import deleteModal from 'components/deleteModal';
+import DeleteModal from 'components/DeleteModal';
 import StarRating from 'react-star-rating';
 import WidgetReviewEdit from './WidgetReviewEdit';
 import cx from './UserCardReviews.styl';
@@ -48,7 +48,7 @@ export default class UserCardReviews extends Component {
 
             {
                 currentDeleteReview ?
-                    <deleteModal
+                    <DeleteModal
                         object={(books[currentDeleteReview.bookId] ? books[currentDeleteReview.bookId].title || currentDeleteReview.id : currentDeleteReview.id) + " review"}
                         active={currentDeleteReview ? true : false}
                         onDelete={() => {
